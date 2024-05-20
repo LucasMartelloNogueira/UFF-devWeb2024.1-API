9aed3aaef8ea64f1a0fe738e4abb5191ff6faa0e
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2024-05-20 14:34:35 -0300

    add template inicial da api

---
+++ src/main/java/id/uff/lucasmartello20241/devwebapi/DevWebApiApplication.java
@@ -0,0 +1,13 @@
+package id.uff.lucasmartello20241.devwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/uff/lucasmartello20241/devwebapi/DevWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.uff.lucasmartello20241.devwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
